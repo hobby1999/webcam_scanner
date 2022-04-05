471f851034dd79d4d052e112309e8b5372ab52c1
hobby1999 <794079248@qq.com>
2022-04-06 03:42:13 +0800

    update

--- webcam_scanner/plugins/api_passwdcheck.py
+++ webcam_scanner/plugins/api_passwdcheck.py
@@ -5,7 +5,7 @@ import os
 
 @router.get("/checkpasswd",
 name="账号密码检查",
-description="用于账号密码检查的api，查看是否有弱密码流出,username为用户名参数，filename为文件参数")
+description="用于账号密码检查的api，查看是否有弱密码泄露,username为用户名参数，filename为文件参数")
 async def checkpasswd(username,filename):
     file_tmp_path = filename.rsplit(".",1)[0]
     filepath = f"firmware/extract/{username}/{file_tmp_path}/_{filename}.extracted"
@@ -20,14 +20,26 @@ async def checkpasswd(username,filename):
           for i in filepath_result:
             if i.split('/')[len(i.split('/'))-1] == "passwd":
                 passwd_path_result.append(i)
-                print(passwd_path_result)
             if i.split('/')[len(i.split('/'))-1] == "shadow":
                 shadow_path_result.append(i)
-        f = open(passwd_path_result[0],"rb")
-        for line in f.readlines():
+        if passwd_path_result:
+            passwd_fopen = open(passwd_path_result[0],"r")
+        if shadow_path_result:
+            shadow_fopen = open(shadow_path_result[0],"r")
+        for line in passwd_fopen.readlines():
             passwd_result.append(line.rsplit("\n")[0])
+        for line in shadow_fopen.readlines():
+            shadow_result.append(line.rsplit("\n")[0])
+        print(shadow_result)
         for i in passwd_result:
-            print(i.split(':'))
+            if i.split(':')[1] == "":
+                return {"code":"200","status":True,"msg":"高危！密码为空","datetime":getcurrenttime()}
+            elif i.split(':')[1] == "*":
+                return {"code":"200","status":True,"msg":"密码经过加密","datetime":getcurrenttime()}
+            elif i.split(':')[1] == "!":
+                return {"code":"200","status":True,"msg":"密码经过加密","datetime":getcurrenttime()}
+            elif i.split(':')[1] == "x":
+                return {"code":"200","status":True,"msg":"开始查找密码","datetime":getcurrenttime()}
     except:
-        pass
+        return {"code":"500","status":False,"msg":"未知错误","datetime":getcurrenttime()}
     pass
