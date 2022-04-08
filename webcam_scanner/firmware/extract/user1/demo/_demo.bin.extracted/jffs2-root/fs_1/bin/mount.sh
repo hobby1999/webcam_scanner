b7f0033afea56e1b032bde12ac0da7c6b91e70da
hobby1999 <794079248@qq.com>
2022-04-08 15:49:05 +0800

    update

--- webcam_scanner/firmware/extract/user1/demo/_demo.bin.extracted/jffs2-root/fs_1/bin/mount.sh
+++
@@ -1,1 +0,0 @@
-mount -o nolock,rw 10.0.0.167:/home/xuxuequan/Ingenicwork/sharenfs /mnt
--- webcam_scanner/firmware/extract/user1/demo/_demo.bin.extracted/jffs2-root/fs_1/bin/webrtc_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sleep 3
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp
-/system/bin/webrtc_stream &
